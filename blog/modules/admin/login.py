5319d7613f35c45150f5af78a0142a6387baba62
xuanli <xuanlia199@163.com>
2018-10-05 15:47:54 +0800

    给flask表单添加了忘了添加的提交按钮

--- blog/modules/admin/login.py
+++ blog/modules/admin/login.py
@@ -1,13 +1,14 @@
 from flask import render_template
 from flask_admin import AdminIndexView, expose
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
     username = StringField("输入昵称", validators=[DataRequired()], render_kw={"placeholder": "请输入用户名"})
     password = PasswordField("输入密码", validators=[DataRequired()], render_kw={"placeholder": "请输入密码"})
+    submit = SubmitField("登录")
 
 
 class IndexView(AdminIndexView):
