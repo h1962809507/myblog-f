1a7288a1f5f8cedca211e553ac2a70c99ebf74d7
xuanli <xuanlia199@163.com>
2018-10-12 14:18:12 +0800

    统计阅读量

--- blog/modules/article/views.py
+++ blog/modules/article/views.py
@@ -1,22 +1,26 @@
 import markdown
 from flask import render_template, current_app, abort
+
+from blog import db
 from blog.models import Article
 from . import article_blu
 
 
 @article_blu.route("/article/<int:code>")
 def detail(code):
-    article = Article.query.get(code)
-    articles = list()
+    article = ""
     try:
-        articles = Article.query.order_by(Article.create_time.desc()).all()[:4]
+        article = Article.query.get(code)
+        article.click += 1
+        db.session.commit()
     except Exception as e:
         current_app.logger.error(e)
         abort(500)
+
     article.content = markdown.markdown(article.content,
                                   extensions=[
                                       'markdown.extensions.extra',
                                       'markdown.extensions.codehilite',
                                       'markdown.extensions.toc',
                                   ])
-    return render_template("blog/single.html", article=article, articles=articles)
+    return render_template("blog/single.html", article=article)
